eed869d71c2840598f11074d819eb106ac4dafc2
hollyman2 <hollyboy2000@mail.ru>
2024-04-02 08:04:31 +0300

    development2

--- network/config/settings.py
+++ network/config/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'config.middleware.authrequiredmiddleware.AuthMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
